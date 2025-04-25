7687396c5812f541cb637fea723726b1f9cb1627
JuanMa <gmjm2004@gmail.com>
2025-04-24 20:17:45 -0500

    primer commit backend

---
+++ src/main/java/edu/ucaldas/back/FrameworksJsApplication.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworksJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworksJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ucaldas/back/FrameworksJsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworksJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
